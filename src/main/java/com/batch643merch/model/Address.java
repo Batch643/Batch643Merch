c08f270f9d5b351a25cd1c299a5dc2126f381692
Caraiza <carlos.araiza96@gmail.com>
2019-11-07 11:34:33 -0600

    User and Address Models

---
+++ src/main/java/com/batch643merch/model/Address.java
@@ -0,0 +1,11 @@
+package com.batch643merch.model;
+
+public class Address {
+	private int userId;
+	private String street1;
+	private String street2;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String Country;
+}
--- src/main/java/com/batch643merch/model/User.java
+++ src/main/java/com/batch643merch/model/User.java
@@ -1,5 +1,83 @@
 package com.batch643merch.model;
 
 public class User {
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+	private Boolean userIsAdmin;
+	private Address userAddress;
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	public User(int userId, String userName, String userEmail, String userPassword, Boolean userIsAdmin,
+			Address userAddress) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+		this.userIsAdmin = userIsAdmin;
+		this.userAddress = userAddress;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public Boolean getUserIsAdmin() {
+		return userIsAdmin;
+	}
+
+	public void setUserIsAdmin(Boolean userIsAdmin) {
+		this.userIsAdmin = userIsAdmin;
+	}
+
+	public Address getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(Address userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + ", userPassword="
+				+ userPassword + ", userIsAdmin=" + userIsAdmin + ", userAddress=" + userAddress + "]";
+	}
+	
+	
+	
 }
